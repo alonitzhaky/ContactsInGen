c2a637508395ba32c8aeb58e223046b4ce8a6f5a
SudarshanKashyapThorve <58326820+SudarshanKashyapThorve@users.noreply.github.com>
2022-09-01 00:40:10 +0530

    Updated with Typing and Check

--- LoadAndSave.py
+++ LoadAndSave.py
@@ -1,9 +1,10 @@
 import json
+import os
 
-def load_file(DATA_FILE): # Loading JSON file with all information
-    with open (DATA_FILE) as file:
+def load_file(contacts_path: str): # Loading JSON file with all information
+    with open(os.path.relpath(contacts_path)) as file:
         return json.load(file)
 
-def save_file(json_list, DATA_FILE): 
-    with open (DATA_FILE, "w") as file: 
-        json.dump(json_list, file)
+def save_file(contacts: list, contacts_path: str): 
+    with open (os.path.relpath(contacts_path), "w") as file: 
+        json.dump(contacts, file, indent = 4)
